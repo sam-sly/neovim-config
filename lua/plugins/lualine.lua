462269894cda597fd0632c20b11734c118b19885
sam-sly <samsly7@gmail.com>
2023-10-06 16:20:52 -0600

    Configured lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,29 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  opts = {
+    theme = 'tokyonight-storm',
+    component_separators = '',
+    section_separators = '',
+    sections = {
+      lualine_a = {'mode'},
+      lualine_b = {
+        { 'filetype', icon_only = true },
+        'filename'
+      },
+      lualine_c = {
+        { 'diagnostics', sources = { 'nvim_diagnostic', 'nvim_lsp', 'vim_lsp' } }
+      },
+      lualine_x = {'diff'},
+      lualine_y = {'branch'},
+      lualine_z = {'location'}
+    },
+    extensions = {
+      'fugitive',
+      'fzf',
+      'lazy',
+      'neo-tree',
+      'trouble'
+    }
+  },
+  config = true
+}
