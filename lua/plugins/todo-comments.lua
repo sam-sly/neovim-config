6621f6dfcbc47c92bff1f428dc725298a515ec11
sam-sly <samsly7@gmail.com>
2023-10-06 23:15:09 -0600

    Added keymaps to move between todo comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,10 +1,10 @@
 return {
   'folke/todo-comments.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    require('todo-comments').setup()
-    -- Todo Keymaps
-    local keymap = vim.keymap
-    keymap.set('n', '<leader>ft', ':TodoTelescope<cr>', { desc = 'Find todos', silent = true })
-  end
+  keys = {
+    { '<leader>ft', ':TodoTelescope<cr>', desc = 'Find todo comments' },
+    { ']t', function() require('todo-comments').jump_next() end, desc = "Next todo comment" },
+    { '[t', function() require('todo-comments').jump_prev() end, desc = "Previous todo comment" }
+  },
+  config = true
 }
