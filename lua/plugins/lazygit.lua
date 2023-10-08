4c79d56913306317977cfd19e8971f3784e786d0
sam-sly <samsly7@gmail.com>
2023-10-08 17:46:27 -0600

    Made fugitive a dependency of lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -2,7 +2,8 @@ return {
   "kdheepak/lazygit.nvim",
   -- optional for floating window border decoration
   dependencies = {
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
+    "tpope/vim-fugitive"
   },
   keys = {
     { '<leader>g', ':LazyGit<cr>', desc = "LazyGit" }
