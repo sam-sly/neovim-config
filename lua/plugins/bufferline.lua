7c9de7ec32ca882d54e12792752f9d344d7eca11
sam-sly <samsly7@gmail.com>
2023-10-08 17:56:28 -0600

    Fixed bufferline diagnostics display

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -11,7 +11,7 @@ return {
           for e, n in pairs(diagnostics_dict) do
             local sym = e == "error" and " "
             or (e == "warning" and " " or "󰌶 ")
-            s = s .. sym .. n
+            s = s .. sym .. n .. ' '
           end
           return s
         end,
