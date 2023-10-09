3f3392b9bfffb359ac7a40cc4cde3ad8bd7b0dec
sam-sly <samsly7@gmail.com>
2023-10-08 21:50:50 -0600

    Reconfigured none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,7 +10,7 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.eslint_lsp,
         null_ls.builtins.completion.spell,
       },
     })
