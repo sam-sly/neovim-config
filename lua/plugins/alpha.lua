bcfff0573aec38bc764823b806a0ecb17b0b5b7e
sam-sly <samsly7@gmail.com>
2023-10-08 20:50:12 -0600

    Added alpha plugin keymap

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,6 +1,10 @@
 return {
     'goolord/alpha-nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    lazy = false,
+    keys = {
+      { '<leader>a', ':Alpha<cr>', desc = 'Alpha startup', silent = true }
+    },
     config = function ()
         require'alpha'.setup(require'alpha.themes.startify'.config)
     end
