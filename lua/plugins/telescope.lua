bcaadcc5ac5ffd1bf87445f4bc0d52ee58ad53cf
sam-sly <samsly7@gmail.com>
2023-10-08 18:05:11 -0600

    Reverted Telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,14 +2,16 @@ return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
   dependencies = {
-    'nvim-lua/plenary.nvim'
+    'nvim-lua/plenary.nvim',
   },
-  keys = {
-    { '<leader>ff', ':Telescope git_files<cr>', desc = "Find git files", silent = true },
-    { '<leader>fF', ':Telescope find_files<cr>', desc = "Find files", silent = true },
-    { '<leader><Space>', ':Telescope find_files<cr>', desc = "Find files", silent = true },
-    { '<leader>fg', ':Telescope live_grep<cr>', desc = "Find grep string", silent = true },
-    { '<leader>fs', ':Telescope grep_string<cr>', desc = "Find string under cursor", silent = true }
-  },
-  config = true
+  config = function()
+    -- Telescope Keymaps
+    local keymap = vim.keymap
+    local builtin = require('telescope.builtin')
+    keymap.set('n', '<leader>ff', builtin.git_files, { desc = "Find git files" })
+    keymap.set('n', '<leader>fF', builtin.find_files, { desc = "Find files" })
+    keymap.set('n', '<leader><Space>', builtin.find_files, { desc = "Find files" })
+    keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Find grep string" })
+    keymap.set('n', '<leader>fs', builtin.grep_string, { desc = "Find string under cursor" })
+  end
 }
