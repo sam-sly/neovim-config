cc3f1797acb8b8e917f2a2c1a0e91e7f7910613d
sam-sly <samsly7@gmail.com>
2023-10-21 19:27:14 -0600

    Added keybinds to icon-picker plugin

--- lua/plugins/icon-picker.lua
+++ lua/plugins/icon-picker.lua
@@ -3,9 +3,16 @@ return {
   dependencies = {
     'stevearc/dressing.nvim'
   },
+  event = 'InsertEnter',
+  keys = {
+    { '<leader>fi', ':IconPickerNormal<cr>', desc = "Find icon/emoji", silent = true }
+  },
   config = function()
     require("icon-picker").setup({
       disable_legacy_commands = true
     })
+
+    -- Icon-Picker keymaps
+    vim.keymap.set('i', '<C-e>', '<cmd>IconPickerInsert<cr>', { desc = 'Find icon/emoji', silent = true, noremap = true })
   end
 }
