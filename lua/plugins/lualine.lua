48f89373c2f67ca98ea2068c894e1508f3d28e90
sam-sly <samsly7@gmail.com>
2023-10-21 19:30:13 -0600

    Modified lualine display

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,36 +1,35 @@
 return {
   'nvim-lualine/lualine.nvim',
+  event = 'BufEnter',
   opts = {
     theme = 'tokyonight-storm',
     options = { component_separators = { left = '', right = '' } },
     sections = {
-      lualine_a = {'mode'},
+      lualine_a = {
+        'mode',
+      },
       lualine_b = {
-        { 'diagnostics', sources = { 'nvim_lsp' } },
-        { 'filetype', icon_only = true, padding = { left = 1, right = 0 } },
-        { 'filename', symbols = {
-          modified = "",
-          newfile = "",
-          readonly = "",
-          unnamed = ""
-        }},
+        { 'location', padding = { left = 1, right = 1 } },
       },
       lualine_c = {
-        {
-          function() return require("nvim-navic").get_location() end,
-          cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
-        }
+        { 'diagnostics', sources = { 'nvim_lsp' } },
       },
-      lualine_x = {'diff'},
-      lualine_y = {'branch'},
-      lualine_z = {{ 'location', padding = { left = 0, right = 0.5 } }}
+      lualine_x = {
+        'diff',
+      },
+      lualine_y = {
+        { 'filetype', icon_only = true, padding = { left = 1, right = 0 } },
+        { 'filename', file_status = false, symbols = { modified = "", newfile = "", readonly = "", unnamed = "" }, },
+      },
+      lualine_z = {
+        'branch',
+      }
     },
     extensions = {
       'fugitive',
       'fzf',
       'lazy',
-      'neo-tree',
-      'trouble'
+      'trouble',
     }
   },
   config = true
