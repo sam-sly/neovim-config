5ba6f242c0f052be316d0e9ff50cf8cf6725a4f9
sam-sly <samsly7@gmail.com>
2023-10-21 19:24:45 -0600

    Added alpha-nvim plugin as disabled

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,11 +1,8 @@
 return {
-    'goolord/alpha-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    lazy = false,
-    keys = {
-      { '<leader>a', ':Alpha<cr>', desc = 'Alpha startup', silent = true }
-    },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
+  'goolord/alpha-nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  enabled = false,
+  config = function ()
+    require'alpha'.setup(require'alpha.themes.startify'.config)
+  end
 };
