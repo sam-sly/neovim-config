e42f27e8b8c2d06b6a01c042d0153cb1223f59fc
sam-sly <samsly7@gmail.com>
2023-10-21 19:35:44 -0600

    Moved mini.bufremove plugin config to bufferline dependency

--- lua/plugins/mini-bufremove.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'echasnovski/mini.bufremove',
-  version = false,
-  keys = {
-    { "<leader>X", ":bdelete<cr>", desc = "Close file without saving", silent = true },
-    { "<leader>x", ":w<cr>:bd<cr>", desc = "Write file and close", silent = true },
-    { "<leader>bc", ":w<cr>:bd<cr>", desc = "Write file and close", silent = true }
-  }
-}
