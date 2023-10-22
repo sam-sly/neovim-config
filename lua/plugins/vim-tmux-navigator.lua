905d1f5bc40bf7bd9b4778f4c65c6e3622c07b7f
sam-sly <samsly7@gmail.com>
2023-10-21 19:34:05 -0600

    Simplified vim-tmux-navigator keymap config

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -1,12 +1,9 @@
 return {
   'christoomey/vim-tmux-navigator',
-  config = function()
-    local keymap = vim.keymap
-    -- tmux navigator
-    vim.g.tmux_navigator_no_mappings = 1
-    keymap.set("n", "<C-h>", ":TmuxNavigateLeft<cr>", { silent = true })
-    keymap.set("n", "<C-j>", ":TmuxNavigateDown<cr>", { silent = true })
-    keymap.set("n", "<C-k>", ":TmuxNavigateUp<cr>", { silent = true })
-    keymap.set("n", "<C-l>", ":TmuxNavigateRight<cr>", { silent = true })
-  end
+  keys = {
+    { "<C-h>", ":TmuxNavigateLeft<cr>", silent = true },
+    { "<C-j>", ":TmuxNavigateDown<cr>", silent = true },
+    { "<C-k>", ":TmuxNavigateUp<cr>", silent = true },
+    { "<C-l>", ":TmuxNavigateRight<cr>", silent = true },
+  },
 }
