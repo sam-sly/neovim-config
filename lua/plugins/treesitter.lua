527e16bbbe37919e4d8b817ffa4c409b449318a4
sam-sly <samsly7@gmail.com>
2023-10-21 19:36:23 -0600

    Updated nvim-treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,46 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  opts = {
-    ensure_installed = {
-      'bash',
-      'c',
-      'lua',
-      'luadoc',
-      'luap',
-      'vim',
-      'vimdoc',
-      'query',
-      'html',
-      'javascript',
-      'json',
-      'jsdoc',
-      'regex',
-      'python',
-      'markdown',
-      'markdown_inline'
-    },
-    auto_install = true
+  build = ':TSUpdate',
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      textobjects = {
+        move = {
+          enable = true,
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+        },
+      },
+      ensure_installed = {
+        'bash',
+        'c',
+        'lua',
+        'luadoc',
+        'luap',
+        'vim',
+        'vimdoc',
+        'query',
+        'html',
+        'javascript',
+        'json',
+        'jsdoc',
+        'typescript',
+        'regex',
+        'python',
+        'markdown',
+        'markdown_inline'
+      },
+      auto_install = true
+    }
   }
-}
