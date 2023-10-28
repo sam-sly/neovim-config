a712558a839e0374e72500df86202dcc7bc29af8
sam-sly <samsly7@gmail.com>
2023-10-28 15:50:23 -0600

    Added lsp doc borders

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -31,8 +31,12 @@ return {
         command_palette = true, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
+        lsp_doc_border = true, -- add a border to hover docs and signature help
       },
+      smart_move = {
+        enabled = true,
+        excluded_filetypes = {"notify"},
+      }
     })
   end
 }
